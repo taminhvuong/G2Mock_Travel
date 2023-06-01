36261c09c97d29714a3f0dfbff86a02dc1bb28bc
TranHoaiThu20 <93358914+taminhvuong@users.noreply.github.com>
2023-06-01 21:28:50 +0700

    commit trip

--- BackEnd/Travel_Mock2302/src/main/java/com/example/travel_mock2302/controller/TripController.java
+++ BackEnd/Travel_Mock2302/src/main/java/com/example/travel_mock2302/controller/TripController.java
@@ -4,6 +4,8 @@ import com.example.travel_mock2302.dto.TourDto;
 import com.example.travel_mock2302.dto.TripDto;
 import com.example.travel_mock2302.entity.Tour;
 import com.example.travel_mock2302.entity.Trip;
+import com.example.travel_mock2302.form.FormCreateTrip;
+import com.example.travel_mock2302.form.FormUpdateTrip;
 import com.example.travel_mock2302.service.TourService;
 import com.example.travel_mock2302.service.TripService;
 import org.modelmapper.ModelMapper;
@@ -11,9 +13,7 @@ import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,10 +24,24 @@ public class TripController {
     private TripService tripService;
     @Autowired
     private ModelMapper modelMapper;
+
     @GetMapping("")
-    public ResponseEntity<?> findAll(){
-        List<Trip> trips= tripService.findAll();
-        List<TripDto> tripDtos=modelMapper.map(trips,new TypeToken<List<TripDto>>(){}.getType());
+    public ResponseEntity<?> findAll() {
+        List<Trip> trips = tripService.findAll();
+        List<TripDto> tripDtos = modelMapper.map(trips, new TypeToken<List<TripDto>>() {
+        }.getType());
         return new ResponseEntity<>(tripDtos, HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    private ResponseEntity<?> saveTrip(FormCreateTrip formCreateTrip) {
+        tripService.saveTrip(formCreateTrip);
+        return new ResponseEntity<>("Sucess Add", HttpStatus.CREATED);
+    }
+
+    @PutMapping("update/{id}")
+    public ResponseEntity<?> updateTrip(@PathVariable String codeTrip, FormUpdateTrip formUpdateTrip) {
+        tripService.updateByCodeTrip(codeTrip, formUpdateTrip);
+        return new ResponseEntity<>("Update", HttpStatus.OK);
     }
 }
